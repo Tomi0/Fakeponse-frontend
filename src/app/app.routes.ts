78fd7d91552630615f3a9484151fe8fea89044d9
Tomi0 <tomasbuendiaalacid@gmail.com>
2026-04-03 11:43:56 +0200

    New pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { WelcomePage } from './domain/welcome-page/welcome-page.component';
+import { HeaderLayout } from './infrastructure/layout/header-layout/header-layout';
+import { ListEndpointsPage } from './domain/list-endpoints-page/list-endpoints-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: WelcomePage
+  },
+  {
+    path: 'admin',
+    component: HeaderLayout,
+    children: [
+      { path: 'endpoints', component: ListEndpointsPage },
+    ]
+  }
+];
---
+++ src/app/domain/list-endpoints-page/list-endpoints-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-endpoints-page',
+  imports: [],
+  templateUrl: './list-endpoints-page.component.html',
+  styleUrl: './list-endpoints-page.component.css',
+})
+export class ListEndpointsPage {}
